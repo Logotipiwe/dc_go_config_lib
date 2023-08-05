19310ee85781437cc0438fdbe1b12fe33d6da3cb
logotipiwe <germna.reus@bk.ru>
2023-08-05 13:24:32 +0600

    Init v0.0.1

---
+++ go.mod
@@ -0,0 +1,4 @@
+module github.com/logotipiwe/dc_go_config_lib
+
+go 1.20
+
---
+++ src/model.go
@@ -0,0 +1,10 @@
+package src
+
+type DcPropertyDto struct {
+	Id          string `json:"id"`
+	Name        string `json:"name"`
+	Value       string `json:"value"`
+	NamespaceId string `json:"namespaceId"`
+	ServiceId   string `json:"serviceId"`
+	Active      bool   `json:"active"`
+}
